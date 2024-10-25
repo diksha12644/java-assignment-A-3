4a684acde34141d1333a5a6e220fc9bc7307b78a
Diksha samyal <dixasamyal1998@gmail.com>
2024-10-25 20:55:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+// //.................................Question 1 ..................................................
+
+// // let n = 5;
+// // for (let i = 1; i <= n; i++) {
+// //     let pattern = '';
+// //     for (let j = 1; j <= i; j++) {
+// //         pattern += '* ';
+// //     }
+// //     console.log(pattern);
+// // }
+
+// //....................................Question 2............................................................
+
+
+// // for (let i = 1; i <= 45; i++) {
+   
+// //     if (i % 3 === 0 && i % 5 === 0) {
+// //         console.log("FizzBuzz");
+// //     }
+  
+// //     else if (i % 3 === 0) {
+// //         console.log("Fizz");
+// //     }
+    
+// //     else if (i % 5 === 0) {
+// //         console.log("Buzz");
+// //     }
+   
+// //     else {
+// //         console.log(i);
+// //     }
+// // }
+
+
+
+// //........................................QUESTION 3................................................
+
+
+// // var numbersArray = [1, 13, 22, 123, 49];
+// // var sum = 0;
+// // for (let i = 0; i < numbersArray.length; i++) {
+// //     sum += numbersArray[i];
+// // }
+
+// // console.log("The sum of the array is:", sum);
+
+// //..............................................question 4...........................................
+
+// // var arr_1 = [3, 5, 22, 5, 7, 2, 45, 75, 89, 21, 2]; 
+// // var arr_2 = [9, 2, 42, 55, 71, 22, 4, 5, 90, 25, 26]; 
+// // var sum1 = 0;
+// // var sum2 = 0;
+
+// // for (var i = 0; i < arr_1.length; i++) {
+// //     sum1 += arr_1[i];
+// // }
+// // for (var i = 0; i < arr_2.length; i++) {
+// //     sum2 += arr_2[i];
+// // }
+
+// // var totalSum = sum1 + sum2;
+
+// // console.log(`${sum1} + ${sum2} = ${totalSum}`);
+
+
+
+// //.......................................QUESTION 5.........................................................
+
+// let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+
+// // Output elements in reverse order, each on a new line
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     console.log(arr[i]);
+// }
+
+
